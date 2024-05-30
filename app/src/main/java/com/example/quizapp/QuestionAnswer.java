a3f92bd6025ec6dad0539536b75788499ed2e6d0
Varsha-67 <22BQ1A0567@vvit.net>
2024-05-30 21:30:55 +0530

    Quiz App

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String Questions []={
+            "What is the capital of France?",
+            "How many continents are there in the world?",
+            "Who wrote the play 'Romeo and Juliet'?",
+            "What is the chemical symbol for water?",
+            "What is the largest planet in our solar system?",
+    };
+    public static String Choices [][]={
+            {"Paris", "Madrid", "Berlin", "Rome"},
+            {"Five", "Six", "Seven", "Eight"},
+            {"Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"},
+            {"CO₂", "O₂", "H₂O", "NaCl"},
+            {"Earth", "Mars", "Jupiter", "Saturn"}
+    };
+    public static String Answers []={
+            "Paris",
+            "Seven",
+            "William Shakespeare",
+            "H₂O",
+            "Jupiter"
+    };
+}
